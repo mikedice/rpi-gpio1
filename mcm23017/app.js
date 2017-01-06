b76deb6adf37ee9d6bf59ee8e0624c9a4112b5b4
Michael Dice <mikedice@MikeDice-MacBook-Air.local>
2017-01-05 19:27:02 -0800

    update

--- mcm23017/app.js
+++ mcm23017/app.js
@@ -3,6 +3,6 @@ var address = 0x18; // TODO: address
 var device = '/dev/i2c-1';
 var wire = new i2c(address, device);
 
-write.scan(function(err, data){
+wire.scan(function(err, data){
     console.log('scan callback');
 });
