5dcf15244ded13a0de92f829e1cca052587659fe
Mike Dice <mikedice417@hotmail.com>
2017-01-08 17:07:32 -0800

    adsfadf

--- mcm23017/app.js
+++ mcm23017/app.js
@@ -10,9 +10,36 @@ var data = [
 	0x71,0x72,0x74,0x78  // row 4
 ];
 
-var H = [0xE1,0xE8,0xD1,0xD2,0xD4,0xD8,0xB1,0xB8,0x71,0x78]
-var h = [0xE1,0xD1,0xB1,0xB2,0xB4,0x71,0x74];
-var a = [0xD1, 0xD2, 0xD4, 0xB1, 0xB4, 0x71, 0x72, 0x74,0x78];
+var H = [0xE1,          0xE8,
+         0xD1,0xD2,0xD4,0xD8,
+		 0xB1,          0xB8,
+		 0x71,          0x78];
+
+var A = [0xE1,0xE2,0xE4,0xE8,
+         0xD1,          0xD8,
+		 0xB1,0xB2,0xB4,0xB8,
+		 0x71,          0x78];
+
+var I = [0xE1,0xE2,0xE4,    
+              0xD2,
+			  0xB2,
+	     0x71,0x72,0x74];
+
+var L = [0xE1,
+         0xD1,
+		 0xB1,
+		 0x71,0x72,0x73];
+
+var E = [0xE1,0xE2,0xE4,
+         0xD1,
+		 0xB1,0xB2,
+		 0x71,0x72,0x74];
+
+var Y = [0xE1,         0xE8,
+              0xD2,0xD4,
+			  0xB2,
+			  0x72];
+
 var blank = [0xE0, 0xD0, 0xB0, 0x70];
 
 console.log('wire created');
@@ -41,10 +68,28 @@ function displayChar(chr, idx, duration, callback){
 wire.writeBytes(0x00, [0x00], function(){
     console.log('wrote first bytes');
 	var duration=1250;
-    displayChar(h, 0, duration, function(){
+    displayChar(H, 0, duration, function(){
 		displayChar(blank, 0, 1, function(){
-			displayChar(a, 0, duration, function(){
-				displayChar(blank, 0, duration, function(){});
+			displayChar(A, 0, duration, function(){
+				displayChar(blank, 0, 1, function(){
+					displayChar(I, 0, duration, function(){
+						displayChar(blank, 0, 1, function(){
+							displayChar(L, 0, duration, function(){
+								displayChar(blank, 0, 1, function(){
+									displayChar(E, 0, duration, function(){
+										displayChar(blank, 0, 1, function(){
+											displayChar(Y, 0, duration, function(){
+												displayChar(blank, 0, 1, function(){
+
+												});
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				});
 			});
 		});	
 	});
